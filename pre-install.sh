3cdb5b6a361d65ff3a46df857b836a8359861521
Nick McCarthy <nick@nickmcc.com>
2023-12-13 13:44:02 -0600

    Update zsh install

--- pre-install.sh
+++ pre-install.sh
@@ -9,15 +9,8 @@ else
   brew update
 fi
 
-# Check if oh-my-zsh is installed
-OMZDIR="$HOME/.oh-my-zsh"
-if [ ! -d "$OMZDIR" ]; then
-  echo 'Installing oh-my-zsh'
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-  echo 'Updating oh-my-zsh'
-  upgrade_oh_my_zsh
-fi
+echo 'Installing oh-my-zsh'
+/bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Change default shell
 if [! $0 = "-zsh"]; then
