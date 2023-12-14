e2aec5b42cd49e9f5ee721e225e5fe43eb74f412
Nick <2506116+k5njm@users.noreply.github.com>
2023-12-13 18:23:19 -0600

    Fix iterm2

--- pre-install.sh
+++ pre-install.sh
@@ -17,7 +17,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export PATH=/opt/homebrew/bin:$PATH && brew bundle
 
     # Specify the preferences directory
-    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$SCRIPTPATH/iterm2/"
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$SCRIPTPATH/iterm2"
     # Tell iTerm2 to use the custom preferences in the directory
     defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
